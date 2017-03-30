191efd610690e0c19159f2324f8719dc619abe4b
sdgu <sdgu000@gmail.com>
2017-03-30 17:57:08 -0400

    some routing update

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+// var gp = require("./routes/gp");
 
 var app = express();
 
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'views/gp')));
 
 app.use('/', index);
 app.use("/gp", index);
+app.use("/gp/", index);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
